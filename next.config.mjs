e095fd2b8388d9f1ebc8d26170c193492b7fc7ab
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-10-10 04:37:28 +0700

    Feat : Tambahkan Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-            port: '',
-            pathname: '/df6nytit6/image/upload/**',
-          },
-        ],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/beranda",
+        permanent: false,
       },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/df6nytit6/image/upload/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
